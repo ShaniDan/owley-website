5043f229fd2d2d7409ffd470ad78e64a41c7aaba
Al <dandavisjs@protonmail.com>
2024-02-19 15:13:45 -0800

    Added a logo and configured newsletters

--- components/HomePageDemo3/MainBanner.js
+++ components/HomePageDemo3/MainBanner.js
@@ -31,7 +31,7 @@ const MainBanner = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = `/api/subscribe`;
+      const url = `${baseUrl}/api/subscribe`;
       const { email } = subscribe;
       const payload = { email };
       const response = await axios.post(url, payload);
